50e2df19908404cce61f998754dbd4918507829f
yiyuyan <yiyuyan>
2024-08-05 20:20:01 +0800

    init

---
+++ src/main/java/cn/ksmcbrigade/stf/StartupTimeForge.java
@@ -0,0 +1,8 @@
+package cn.ksmcbrigade.stf;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("stf")
+public class StartupTimeForge {
+}
---
+++ src/main/java/cn/ksmcbrigade/stf/mixin/MinecraftMixin.java
@@ -0,0 +1,36 @@
+package cn.ksmcbrigade.stf.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.toasts.SystemToast;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.resources.language.LanguageManager;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.lang.management.ManagementFactory;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+
+    @Shadow @Final private ToastComponent toast;
+    @Shadow @Final private LanguageManager languageManager;
+    @Unique
+    public boolean startupTimeForge$startup = true;
+
+    @Inject(method = "onGameLoadFinished",at = @At("TAIL"))
+    public void finished(CallbackInfo ci){
+        if(!startupTimeForge$startup) return;
+
+        long timeMillis = ManagementFactory.getRuntimeMXBean().getUptime();
+        float time = ((float) (timeMillis / 1000.0D));
+        Component title = Component.translatable("stf.time",Float.parseFloat(String.format("%.2f",time).replace("Format error:","")));
+        SystemToast.addOrUpdate(this.toast, SystemToast.SystemToastIds.PERIODIC_NOTIFICATION, title,this.languageManager.getSelected().equals("zh_cn")?(time*3F>100F?Component.nullToEmpty("打败了全国"+String.format("%.2f",time/3F)+"%的玩家"):Component.nullToEmpty("打败了全国"+String.format("%.2f",time*3F)+"%的玩家")):Component.empty());
+        startupTimeForge$startup = false;
+    }
+}
